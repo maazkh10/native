5a88ff9c51771fe10532eaba5da7a990e28e35ad
manish-mahto-982 <manish.mahto@balancenutrition.in>
2024-02-01 17:03:11 +0530

    manish- otp screen done

--- components/OTPInput.jsx
+++ components/OTPInput.jsx
@@ -1,25 +1,65 @@
 import React, { useState } from "react";
-import { StyleSheet, TextInput } from "react-native";
+import { Keyboard, StyleSheet, TextInput, View } from "react-native";
+import { theme } from "../assets/theme";
 
-function OTPInput() {
+function OTPInput({ ...props }) {
   const [isFocused, setFocused] = useState(false);
+
+
   return (
-    <TextInput
-      placeholder="Enter text here"
-          style={[styles.input, {
-            borderColor: isFocused ? "green" : "blue",
-            borderWidth: 1,
-      }]}
-      onFocus={() => setFocused(true)}
-      onBlur={() => setFocused(false)}
-    />
+    <View
+      style={[
+        styles.inputWrapper,
+        {
+          borderColor: isFocused
+            ? "rgba(167, 32, 36, 0.3)"
+            : theme.colors.border,
+          borderWidth: 1,
+          backgroundColor: props.value.length
+            ? theme.colors.primaryRed
+            : "transparent",
+        },
+      ]}
+    >
+      <TextInput
+        placeholder="0"
+        placeholderTextColor={
+          isFocused ? "rgba(167, 32, 36, 0.3)" : "rgba(0,0,0,0.1)"
+        }
+        // keyboardType="numeric"
+        
+        cursorColor={"rgba(0,0,0,0.1)"}
+        maxLength={1}
+        style={[
+          styles.input,
+          { color: props.value.length ? "#fff" : theme.colors.primaryRed },
+        ]}
+        onFocus={() => {
+          setFocused(true);
+        }}
+        onBlur={() => {
+          setFocused(false);
+        }}
+        {...props}
+      />
+    </View>
   );
 }
 
 export default OTPInput;
 
 const styles = StyleSheet.create({
-    input: {
-      
+  inputWrapper: {
+    width: 48,
+    height: 48,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 1,
+  },
+  input: {
+    textAlign: "center",
+    fontSize: 32,
+    fontWeight: "bold",
   },
 });
