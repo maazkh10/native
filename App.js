c1de7331339713106aa00cad5281199a081ea4b0
manish-mahto-982 <manish.mahto@balancenutrition.in>
2024-02-01 12:23:10 +0530

    manish- folder structure created

--- App.js
+++ App.js
@@ -1,23 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Platform, SafeAreaView } from 'react-native';
-import Intro from './pages/intro';
-import Home from './pages/Home';
-export default function App() {
+import React, { Component } from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  Animated,
+  TouchableOpacity,
+} from "react-native";
+import Svg from "react-native-svg"; // Import the correct SVG components
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./screens/Home";
+import Signup from "./screens/Signup";
+import Intro from "./screens/Intro";
+import MyMobile from "./screens/MyMobile";
+import OTPScreen from "./screens/OTPScreen";
+import MyProfile from "./screens/MyProfile";
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      {/* <Intro /> */}
-      {/* */}
-     <Home />
-      {/* <StatusBar style="auto" /> */}
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Intro">
+        <Stack.Screen
+          name="Intro"
+          component={Intro}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Signup"
+          component={Signup}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="MyMobile"
+          component={MyMobile}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="OTPScreen"
+          component={OTPScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="MyProfile"
+          component={MyProfile}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  backgroundColor : "white",
-  padding: Platform.OS === 'android' ? StatusBar.currentHeight: 0,
 
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  intro: {
+    alignItems: "center",
+    justifyContent: "center",
+    bottom: 50,
+    backgroundColor: "white",
   },
 });
+
+export default App;
---
+++ screens/MyMobile.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MyMobile = () => {
+  return (
+    <View>
+      <Text>MyMobile</Text>
+    </View>
+  )
+}
+
+export default MyMobile
